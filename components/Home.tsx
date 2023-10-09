99da373299455261072b90e62b53618e076729ef
Michael Schulz <mi.schulz@hotmail.com>
2023-10-09 12:29:56 +0200

    Add comments and reintroduce progressbar

--- components/Home.tsx
+++ components/Home.tsx
@@ -13,41 +13,37 @@ import ScrollDiv from './ui/ScrollDiv';
 
 export default function Home() {
   const [pathIndex, setPathIndex] = useState(0);
-  const [navbarPercentage, setNavbarPercentage] = useState(0);
+  const [progressBarPercentage, setProgressbarPercentage] = useState(0);
   const sectionsRef = useRef<any[]>([]);
 
   useEffect(() => {
     const numberOfSections = document.querySelectorAll('.section').length;
-    // This observer has to be setup, because css-snap doesn't work properly with scroll event listeners. Also, due to each section having a height of 100vh, the scroll event listener doesn't fire when scrolling to the next section, because the section immediately enters the viewport on first page mount.
-    const observer = new IntersectionObserver( // the intersection observer is an inbuilt api that allows us to observe elements in the viewport
+    // This observer has to be setup, because css-snap doesn't work properly with scroll event listeners. Also, due to each section having a height of 100vh, the scroll event listener doesn't fire when scrolling to the next section (which took a bit to figure out), because each section immediately enters the viewport on first page mount.
+    const observer = new IntersectionObserver( // the intersection observer is an inbuilt api that allows to observe elements in the viewport (just putting this here so I don't forget)
       (entries) => {
         entries.forEach((entry) => {
           if (entry.isIntersecting) {
             // entry is intersecting means that the section is in the viewport, which is the case when the section snaps to the top of the viewport
-            // Assuming that the section's id is the same as its index
-            setPathIndex(Number(entry.target.getAttribute('property'))); // we set the path index, which is used to determine which svg path to use in the svgs component
+            setPathIndex(Number(entry.target.getAttribute('property'))); // we set the path index though the property which I specified in the section html tag, which is used to determine which svg path to use in the svgs component
             const percentage =
               (Number(entry.target.getAttribute('property')) + 1) /
               numberOfSections;
-            setNavbarPercentage(percentage * 100); // we set the navbar percentage, which is used to determine the width of the red line in the navbar
-            // console.log('percentage', percentage);
+            setProgressbarPercentage(percentage * 100); // I set the percentage, which is used to determine the width of the progressbar in the ProgressBar component
 
-            entry.target.classList.add('visible'); // we set the opacity of the section to 1, which is needed because the opacity is set to 0 in the initial state of the section
+            entry.target.classList.add('visible'); // I set the opacity of the section to 1, which is needed because the opacity is set to 0 in the initial state of the section
 
             if (Number(entry.target.getAttribute('property')) === 1) {
               hideScrollDiv();
+              // the scroll div is used in the hero to signal that the user should scroll down
+              // if the user is past the first section (which has property 0), the scroll div is hidden
             }
-
-            // if (Number(entry.target.getAttribute('property')) === 0) {
-            //   showScrollDiv();
-            // }
           } else {
-            entry.target.classList.remove('visible'); // we remove the visible class from the section, which is needed because the opacity is set to 0 in the initial state of the section
+            entry.target.classList.remove('visible'); // remove the visible class from the section, which is needed because the opacity is set to 0 in the initial state of the section
           }
         });
       },
       {
-        threshold: 0.3, // the threshold is the percentage of the section that has to be in the viewport for the observer to fire. The smaller the threshold, the faster the observer fires, which is why I set it to 0.01
+        threshold: 0.3, // the threshold is the percentage of the section that has to be in the viewport for the observer to fire. The smaller the threshold, the faster the observer fires. The threshold I set now is set so it prevents any bugs when the user has a weird scroll behavior
       },
     );
 
@@ -60,7 +56,7 @@ export default function Home() {
 
   return (
     <div>
-      <Navbar percentage={navbarPercentage} />
+      <Navbar percentage={progressBarPercentage} />
       <div
         className="relative h-screen w-screen main snap-mandatory snap-y overflow-auto"
         style={{ position: 'sticky', top: 0 }}
@@ -109,7 +105,7 @@ export default function Home() {
         </section>
       </div>
       <ScrollDiv />
-      {/* <ProgressBar percentage={navbarPercentage} /> */}
+      <ProgressBar percentage={progressBarPercentage} />
     </div>
   );
 }
--- components/ui/ProgressBar.tsx
+++ components/ui/ProgressBar.tsx
@@ -1,8 +1,16 @@
+import { ColorThemeContext } from '@/context/ColorThemeContextProvider';
+import { useContext } from 'react';
+
 export default function ProgressBar({ percentage }: { percentage: number }) {
+  const colorContext = useContext(ColorThemeContext);
   return (
     <div
-      className="bg-[#429585] fixed bottom-0 left-0 h-3"
-      style={{ width: `${percentage}%`, transition: 'width 0.5s ease-out' }}
+      className=" fixed bottom-0 left-0 h-3"
+      style={{
+        width: `${percentage}%`,
+        transition: 'width 0.5s ease-out',
+        backgroundColor: colorContext.colorThemeColor,
+      }}
     />
   );
 }
