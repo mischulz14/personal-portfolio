600a63bb09a7b144fad6092a8226f0985ec29309
Michael Schulz <mi.schulz@hotmail.com>
2023-10-16 16:15:03 +0200

    Add new button as ui element

---
+++ components/ui/Button.tsx
@@ -0,0 +1,49 @@
+import { ColorThemeContext } from '@/context/ColorThemeContextProvider';
+import { useContext } from 'react';
+
+interface ButtonProps {
+  children: React.ReactNode;
+  type?: 'primary' | 'outlined' | 'secondary';
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  className?: string;
+  disabled?: boolean;
+}
+
+export default function Button({
+  children,
+  type,
+  onClick,
+  disabled,
+}: ButtonProps) {
+  const colorContext = useContext(ColorThemeContext);
+
+  const classNames = () => {
+    if (type === 'primary') {
+      return `hover:scale-105 text-white py-2 px-4 rounded-lg transition-all duration-200`;
+    }
+    if (type === 'secondary') {
+      return `bg-transparent border-2 hover:scale-105 text-white  py-2 px-4 rounded-lg transition-all duration-200`;
+    }
+    if (type === 'outlined') {
+      return `bg-transparent border-[0.5px] hover:scale-105 text-white  py-2 px-4 rounded-lg transition-all duration-200`;
+    }
+  };
+
+  return (
+    <>
+      <button
+        style={{
+          backgroundColor:
+            type === 'primary' ? colorContext.colorThemeColor : 'transparent',
+          borderColor:
+            type === 'secondary' ? colorContext.colorThemeColor : 'white',
+        }}
+        disabled={disabled}
+        onClick={onClick}
+        className={classNames()}
+      >
+        {children}
+      </button>
+    </>
+  );
+}
