6eeb996fa29c96958679bcfc6fb3362863eee721
Michael Schulz <mi.schulz@hotmail.com>
2023-10-19 12:45:15 +0200

    Change uri

--- server/src/apollo-client.ts
+++ server/src/apollo-client.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const createApolloClient = () => {
   return new ApolloClient({
-    uri: 'http://localhost:4000',
+    uri: 'https://personal-portfolio-gi9v3oj56-mischulz14.vercel.app',
     cache: new InMemoryCache(),
   });
 };
