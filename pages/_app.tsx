e0437c26eec229dc90766c0957484172cbc747c2
mischulz14 <mi.schulz@hotmail.com>
2025-07-02 23:09:01 +0200

    remove prisma and appollo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,25 +2,20 @@ import '@/styles/globals.css';
 
 import ColorThemeProvider from '@/context/ColorThemeContextProvider';
 import MemoryCardContextProvider from '@/context/MemoryGameContextProvider';
-import createApolloClient from '@/server/src/apollo-client';
-import { ApolloProvider } from '@apollo/client';
 import { appWithTranslation } from 'next-i18next';
 import type { AppProps } from 'next/app';
 
 import Layout from '../components/layout/Layout';
 
 function App({ Component, pageProps }: AppProps) {
-  const client = createApolloClient();
   return (
-    <ApolloProvider client={client}>
-      <Layout>
-        <ColorThemeProvider>
-          <MemoryCardContextProvider>
-            <Component {...pageProps} />
-          </MemoryCardContextProvider>
-        </ColorThemeProvider>
-      </Layout>
-    </ApolloProvider>
+    <Layout>
+      <ColorThemeProvider>
+        <MemoryCardContextProvider>
+          <Component {...pageProps} />
+        </MemoryCardContextProvider>
+      </ColorThemeProvider>
+    </Layout>
   );
 }
 
